78e0dac2fd4ea45c18f4571e044dece9ad521cce
Sandra Sandoval <sandra_sandoval@brown.edu>
2024-03-15 19:50:31 -0400

    finished studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,20 +10,81 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  // POST REQUEST
+  // URL: https://cab.brown.edu/api/?page=fose&route=search&is_ind_study=N&is_canc=N
+  // URL PARAMS: page=fose & route = promoted
+  //
+
+  const [data, setData] = useState(bakeryData);
+  const [cart, setCart] = useState([]);
+
+  // run when this component is first loaded
+  const loadData = () => {
+    setData([...bakeryData]);
+  };
+
+  useEffect(() => {
+    console.log("component loaded", data.length);
+    loadData();
+  }, []); // run once when the component is first loaded if []
+
+  useEffect(() => {
+    console.log("cart changed: ", cart.length);
+    console.log("data: ", data.length);
+  }, [cart, data]); // if there's a value (or more) in here, run this function every time one of those values changes
+
+  const addToCart = (price, name) => {
+    console.log("adding to cart:", price);
+
+    setCart((prev_cart) => [...prev_cart, { price, name }]);
+  };
+
+  const cartJSX =
+    cart.length === 0 ? (
+      <p>Cart is empty</p>
+    ) : (
+      cart.map((item, index) => (
+        <p key={index}>
+          $ {item.price} {item.name}
+        </p>
+      ))
+    );
+  // const total = cart.map
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      <div id="menu-container">
+        {data.map((item, index) => (
+          <BakeryItem
+            key={index}
+            item={item}
+            onClick={() => addToCart(item.price, item.name)} // Pass addToCart function as onClick prop
+          />
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          setData((prev_data) =>
+            prev_data.filter((item, index) => index % 2 === 0)
+          );
+        }}
+      >
+        filter
+      </button>
+      <button
+        onClick={() => {
+          console.log("restoring data");
+          setData(bakeryData); // original data
+        }}
+      >
+        clear
+      </button>
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cartJSX}
+        <p>Total: $ {totalPrice}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,16 @@
-// TODO: create a component that displays a single bakery item
+// // // TODO: create a component that displays a single bakery item
+import { useState } from "react";
+
+export default function BakeryItem({ item, onClick }) {
+  return (
+    <div className="bakery-item" onClick={onClick}>
+      <h3>{item.name}</h3>
+      <p>{item.description}</p>
+      <p>Price: ${item.price}</p>
+    </div>
+
+    // <p onClick={onClick}>
+    //   Bakery Item: {item.name}, {item.description}, {item.price}
+    // </p>
+  );
+}
